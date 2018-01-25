2e03a97fad441d0fd677435f419ae906e5bb1f75
Aleksandar Filipov <aleksandar.filipov@jayway.com>
2018-01-25 21:35:37 +0100

    Added code to decode respose, all a hack...

---
+++ examples/ReceiveDemo_Advanced_working_biltema/output.ino
@@ -0,0 +1,106 @@
+static const char* bin2tristate(const char* bin);
+static char * dec2binWzerofill(unsigned long Dec, unsigned int bitLength);
+
+void output(unsigned long decimal, unsigned int length, unsigned int delay, unsigned int* raw, unsigned int protocol) {
+
+  if (decimal == 0) {
+    Serial.print("Unknown encoding.");
+  } else {
+    const char* b = dec2binWzerofill(decimal, length);
+    Serial.print("Decimal: ");
+    Serial.print(decimal);
+    Serial.print(" (");
+    Serial.print( length );
+    Serial.print("Bit) Binary: ");
+    Serial.print( b );
+    Serial.print(" Tri-State: ");
+    Serial.print( bin2tristate( b) );
+    Serial.print(" PulseLength: ");
+    Serial.print(delay);
+    Serial.print(" microseconds");
+    Serial.print(" Protocol: ");
+    Serial.println(protocol);
+    {
+      
+      unsigned long decoded = 0UL;
+      //decoded = (0x1FF & 0x0100) >> 8;
+/*      decoded = (decimal & 0x20) >> 5;
+      decoded = decoded | ((decimal & 0x10) >> 3);
+      decoded = decoded | ((decimal & 0x08) >> 1);
+      decoded = decoded | ((decimal & 0x04) << 1);
+      decoded = decoded | ((decimal & 0x80) >> 3);
+      decoded = decoded | ((decimal & 0x40) << 0);
+      decoded = decoded | ((decimal & 0x02) << 5);*/
+
+      decoded = (decimal & 0x20) >> 5;
+      decoded = decoded | ((decimal & 0x10) >> 3);
+      decoded = decoded | ((decimal & 0x08) >> 1);
+      decoded = decoded | ((decimal & 0x04) << 1);
+      decoded = decoded | ((decimal & 0x80) >> 3);
+      decoded = decoded | ((decimal & 0x40) >> 1);
+      decoded = decoded | ((decimal & 0x02) << 5);
+
+
+      Serial.println("decoded ");
+      Serial.println(decoded);
+      Serial.println("decoded minus 41 ");
+      Serial.println(decoded-41UL);
+      Serial.println("decoded try 2 ");
+      Serial.println(decoded-41UL);
+      
+            
+    }
+    
+  }
+  
+  Serial.print("Raw data: ");
+  for (unsigned int i=0; i<= length*2; i++) {
+    Serial.print(raw[i]);
+    Serial.print(",");
+  }
+  Serial.println();
+  Serial.println();
+}
+
+static const char* bin2tristate(const char* bin) {
+  static char returnValue[50];
+  int pos = 0;
+  int pos2 = 0;
+  while (bin[pos]!='\0' && bin[pos+1]!='\0') {
+    if (bin[pos]=='0' && bin[pos+1]=='0') {
+      returnValue[pos2] = '0';
+    } else if (bin[pos]=='1' && bin[pos+1]=='1') {
+      returnValue[pos2] = '1';
+    } else if (bin[pos]=='0' && bin[pos+1]=='1') {
+      returnValue[pos2] = 'F';
+    } else {
+      return "not applicable";
+    }
+    pos = pos+2;
+    pos2++;
+  }
+  returnValue[pos2] = '\0';
+  return returnValue;
+}
+
+static char * dec2binWzerofill(unsigned long Dec, unsigned int bitLength) {
+  static char bin[64]; 
+  unsigned int i=0;
+
+  while (Dec > 0) {
+    bin[32+i++] = ((Dec & 1) > 0) ? '1' : '0';
+    Dec = Dec >> 1;
+  }
+
+  for (unsigned int j = 0; j< bitLength; j++) {
+    if (j >= bitLength - i) {
+      bin[j] = bin[ 31 + i - (j - (bitLength - i)) ];
+    } else {
+      bin[j] = '0';
+    }
+  }
+  bin[bitLength] = '\0';
+  
+  return bin;
+}
+
